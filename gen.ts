f30c71fe16c2930ee426e9b317d33e16c31ed81d
Vitor Soares Silva <silva.vitor.soares@gmail.com>
2022-05-23 23:20:30 -0300

    add request schema

--- gen.ts
+++ gen.ts
@@ -1,32 +1,72 @@
-import { compile } from 'json-schema-to-typescript';
+import { compile, compileFromFile } from 'json-schema-to-typescript';
 import { JSONSchema4 } from 'json-schema';
 import { mkdir, readFile, writeFile } from 'fs/promises';
+import { load } from 'js-yaml';
 import axios from 'axios';
 import path from 'path';
 
-async function schemaFromFile(path: string): Promise<JSONSchema4> {
-  return JSON.parse((await readFile(path)).toString());
+type FileType = 'json' | 'yaml';
+
+const parserByType = {
+  json: JSON.parse,
+  yaml: load,
+} as {
+  [key in FileType]: (text: string) => any;
+};
+
+async function schemaFromFile(
+  path: string,
+  type: FileType
+): Promise<JSONSchema4> {
+  return parserByType[type]((await readFile(path)).toString());
 }
 
 async function schemaFromUrl(url: string): Promise<JSONSchema4> {
   return (await axios(url)).data;
 }
 
-async function generateTSFromSchema(schema: JSONSchema4, name: string, dest: string) {
-  schema.id = name;
-  const compiled = await compile(schema, name, {});
-  await mkdir(path.dirname(dest), {recursive: true})
-  await writeFile(dest, compiled);
+async function generateTSFromSchema(mapping: Mapping) {
+  const schema = await mapping.schema;
+  let name = mapping.name;
+  if (name) {
+    schema.id = name;
+  } else {
+    name = schema.title || schema.id;
+  }
+  if (!name) {
+    throw `No name found for mapping: ${JSON.stringify(mapping)}`;
+  }
+  const compiled = await compile(schema, name, {
+    cwd: mapping.cwd,
+  });
+  await mkdir(path.dirname(mapping.dest), { recursive: true });
+  await writeFile(mapping.dest, compiled);
+}
+
+interface Mapping {
+  schema: Promise<JSONSchema4>;
+  name?: string;
+  dest: string;
+  cwd?: string;
 }
 
 async function gen() {
-  const mappings = [{
-    schema: schemaFromUrl("https://schema.postman.com/json/collection/latest/collection.json"),
-    name: "CollectionDetails",
-    dest: "./generated/postman/collection.ts"
-  }];
+  const mappings: Mapping[] = [
+    {
+      schema: schemaFromUrl(
+        'https://schema.postman.com/json/collection/latest/collection.json'
+      ),
+      name: 'CollectionDetails',
+      dest: './generated/postman/collection.ts',
+    },
+    {
+      schema: schemaFromFile('./schemas/request.yaml', 'yaml'),
+      dest: './generated/types/request.ts',
+      cwd: './schemas/',
+    },
+  ];
   for await (const mapping of mappings) {
-    await generateTSFromSchema(await mapping.schema, mapping.name, mapping.dest);
+    await generateTSFromSchema(mapping);
   }
 }
 
